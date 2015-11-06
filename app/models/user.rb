37665c254aee13b3369f261d614bf2e07e0979fb
John Mark Untalan <apeiron.jmunts@gmail.com>
2015-11-06 10:16:14 +0800

    First commit with Authentication module

---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable, :timeoutable,
+         :authentication_keys => [:login], :timeout_in => 1.hours
+
+  attr_accessor :login
+
+  validates :username,
+  	:presence => true,
+  	:uniqueness => {
+  		:case_sensitive => false
+  	},
+  	:format => {
+  		:with => /\A[a-zA-Z0-9_\.]*\z/
+  	}
+  validate :validate_username
+
+  def validate_username
+  	if User.where(email: username).exists?
+  		errors.add(:username, :invalid)
+  	end
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+	  conditions = warden_conditions.dup
+	  if login = conditions.delete(:login)
+	    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+	  else
+	    if conditions[:username].nil?
+	      where(conditions).first
+	    else
+	      where(username: conditions[:username]).first
+	    end
+	  end
+	end
+end
