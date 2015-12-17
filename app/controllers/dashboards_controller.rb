8fbda4f4255b216f56b9939e7dfcb49aeaa51f44
Kimmie <kanne.degracia@appsource.biz>
2015-12-17 14:23:43 +0800

    Fixed Dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,10 +4,20 @@ class DashboardsController < ApplicationController
 	def index
 		@sale = Sale.update_customer_count
 		@branches = current_brand.branches
+		@customer_count_array = get_customer_count_array(@branches, @sale)
 
+		@chart = LazyHighCharts::HighChart.new('graph') do |f|
+		  f.title(text: "Customer Count")
+		  f.xAxis(categories: @branches.pluck(:name))
+		  f.series(name: "Customer Count", data: @customer_count_array)
+		  f.chart({defaultSeriesType: "bar"})
+		end
+	end
+
+	def get_customer_count_array(branches, sale) 
 	  arr = Array.new
-		@branches.each do |branch|
-			@sale.each do |sale|
+		branches.each do |branch|
+			sale.each do |sale|
 				if sale.branch_id == branch.id 
 					@a = sale.customer_count
 				else
@@ -16,13 +26,8 @@ class DashboardsController < ApplicationController
 			end
 			arr.append(@a)
 		end
-
-		@chart = LazyHighCharts::HighChart.new('graph') do |f|
-		  f.title(text: "Customer Count")
-		  f.xAxis(categories: @branches.pluck(:name))
-		  f.series(name: "Customer Count", data: arr)
-		  f.chart({defaultSeriesType: "bar"})
-		end
+		return arr
 	end
+
 end
 
