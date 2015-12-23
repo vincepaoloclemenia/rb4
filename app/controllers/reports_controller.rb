ee824be7576735a1332b39cfea30e15e17583c49
John Mark Untalan <apeiron.jmunts@gmail.com>
2015-12-23 12:55:15 +0800

    Added pdf to purchase_listings

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -13,6 +13,17 @@ class ReportsController < ApplicationController
 		@q = current_brand.purchases.ransack(params[:q])
 		@purchases = @q.result
 		@suppliers = (current_client.suppliers.pluck(:name,:id) + current_brand.suppliers.pluck(:name,:id)).uniq
+
+		respond_to do |format|
+			format.html
+      format.pdf do
+        render :pdf         	=> "Purchase List Reports",
+                :orientation  => 'Landscape',
+                :page_width   => '13in',
+                :margin => {:top       => 1,
+                             :bottom   => 1} 
+      end
+    end
 	end
 
 	#PRICE MOVEMENT REPORT
