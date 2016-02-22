4666a8c85159cad08e2c216314d9e3f78cd268b8
Roxanne Vargas <rvargas@appsource.biz>
2016-02-22 09:36:51 +0800

    Fixing ICA controller and helper.

--- app/controllers/item_cost_analysis_reports_controller.rb
+++ app/controllers/item_cost_analysis_reports_controller.rb
@@ -3,19 +3,15 @@ class ItemCostAnalysisReportsController < ApplicationController
   before_action :access_control
 
   def index
-    
-    if params[:date_entry].nil?
-      a = Date.today
-    else
-      a = Date.strptime(params[:date_entry], "%m/%d/%Y")
-      a = a - 1.month
+    a = Date.strptime(params[:date_entry], "%m/%d/%Y")
+    a = a - 1.month
     end
     @categories = Category.main
     # a = Date.today - 1.month
     b = Date.today 
     # inventory = Inventory.where("entry_date <= ? AND br∂anch_id = ?", (Date.today - 1.month), 2).last
-    inventory = Inventory.where(entry_date: a.beginning_of_month..a.end_of_month, branch_id: 1).last
-    end_inventory = Inventory.where(entry_date: b.beginning_of_month..b, branch_id: 1).last
+    inventory = Inventory.where(entry_date: a.beginning_of_month..a.end_of_month, branch_id: params[:branch_id]).last
+    end_inventory = Inventory.where(entry_date: b.beginning_of_month..b, branch_id: params[:branch_id]).last
     # @ending_inventory = InventoryItem.where(inventory_items).last
     # @ending_inventory = Inventory.where("entry_date <= ? AND branch_id = ?", Date.today, 2).last
     if inventory.nil? && end_inventory.nil? 
