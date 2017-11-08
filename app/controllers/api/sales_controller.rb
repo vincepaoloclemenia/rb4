90babaf41a712e72a4ab3f62d4a41d286630b63e
Vincent Paolo <vincentpaoloclemenia@gmail.com>
2017-11-08 09:31:45 +0800

    Made Brand model fat instead of controller

--- app/controllers/api/sales_controller.rb
+++ app/controllers/api/sales_controller.rb
@@ -1,6 +1,5 @@
 class Api::SalesController < ApplicationController
     before_action :authenticate_user!
-    before_action :get_total_days, only: [:get_average_revenues, :get_sales_averages, :get_customer_count]
     
     def index        
         @sales = Branch.find(params[:branch_id]).sales.order(sale_date: :desc).paginate(page: params[:page], per_page: 5)
@@ -8,25 +7,19 @@ class Api::SalesController < ApplicationController
     end
 
     def get_sales_averages
-        @last_week_ave_sales =  current_brand.sales.any? || !current_user.brand.nil? ? current_brand.sales.where(sale_date: (Date.today.last_week + 1)..Date.today).pluck(:net_total_sales).sum / @last_week : 0                       
-        @average_sales = current_brand.sales.any? || !current_user.brand.nil? ? current_brand.sales.pluck(:net_total_sales).sum / @total_days : 0        
+        @last_week_ave_sales =  current_brand.get_sales_average                      
+        @average_sales = current_brand.last_week_sales     
     end
 
     def get_customer_count
-        @customer_count = current_brand.sales.any? || !current_user.brand.nil? ? current_brand.sales.where(sale_date: current_brand.created_at.to_date..Date.today).pluck(:customer_count).sum / @total_days : 0 
-        @last_week_count = current_brand.sales.any? || !current_user.brand.nil? ? current_brand.sales.where(sale_date: (Date.today.last_week - 1).. Date.today).map { |s| s.customer_count }.sum / @last_week : 0
+        @customer_count = current_brand.customer_count_average
+        @last_week_count = current_brand.last_week_customer_count
         render json: { customer_count: @customer_count, average_count: @last_week_count }
     end
 
     def get_average_revenues
-        @last_week_revenues = current_brand.sales.any? || !current_user.brand.nil? ? (@settlement_sales + current_brand.sales.pluck(:net_total_sales).sum + current_brand.sales.pluck(:vat).sum + current_brand.sales.pluck(:service_charge).sum) / @last_week : 0
-        @average_revenues = current_brand.sales.any? || !current_user.brand.nil? ? (@settlement_sales + current_brand.sales.pluck(:net_total_sales).sum + current_brand.sales.pluck(:vat).sum + current_brand.sales.pluck(:service_charge).sum) / @total_days : 0
+        @last_week_revenues = current_brand.average_revenues("last_week")
+        @average_revenues = current_brand.average_revenues("daily")
     end
 
-    private
-        def get_total_days
-            @settlement_sales = current_brand.sales.any? || !current_user.brand.nil? ? current_brand.sales.map { |s| s.total_sales_by_settlement_type }.sum : 0  
-            @last_week = ((Date.today - Date.today.last_week) - 1 ).to_i
-            @total_days = current_brand.created_at.to_date == Date.today ? 1 : ( Date.today - current_brand.created_at.to_date ).to_i 
-        end
 end
