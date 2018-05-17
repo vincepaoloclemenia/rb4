a3f0afa8e8f33db086aebe3dc916e248a4ca4920
Vincent Paolo <vincentpaoloclemenia@gmail.com>
2018-05-17 18:12:23 +0800

    --max_old_space_size=4096

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,7 +30,7 @@
 
 window.onload = function(){
   const v8 = require('v8');
-  v8.setFlagsFromString('--max_old_space_size=8192');
+  v8.setFlagsFromString('--max_old_space_size=4096');
 }
 
 function getParameterByName(name) {
