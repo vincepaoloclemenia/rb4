205f36727482b336a04445e82e8736a2a65eafb8
Vincent Paolo <vincentpaoloclemenia@gmail.com>
2018-07-31 18:12:06 +0800

    removed 'remove_column'

--- db/migrate/20180730082848_used_json_b.rb
+++ db/migrate/20180730082848_used_json_b.rb
@@ -1,7 +1,6 @@
 class UsedJsonB < ActiveRecord::Migration
   def change
     enable_extension 'citext'
-    remove_column :brand_settings, :purchase_order_privilege
     add_column :brand_settings, :purchase_order_privilege, :jsonb, null: false, default: '{}'
   end
 end
